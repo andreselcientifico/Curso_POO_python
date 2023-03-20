39ecbb8321150429d9d178dc2154203975a93750
andreselcientifico <andresperezrodriguez28@gmail.com>
2023-03-20 13:18:40 -0500

    busqueda binaria y busqueda lineal

--- funcion_ley_de_la_multiplicacion.py
+++ funcion_ley_de_la_multiplicacion.py
@@ -1,6 +1,9 @@
-def f(n):
-    for i in range(n):
-        for j in range(n*n):
+def f(n , k):
+    for i in n:
+        for j in k:
             print(i, j)
 
-print(f(10))
+print(f(['a','b','c'],[1,2,3]))
+
+my_list = [2,'apple',3.5]
+print(my_list)
