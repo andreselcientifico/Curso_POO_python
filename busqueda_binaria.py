63ec2587102a724cda274db1291dd62b211783d2
andreselcientifico <andresperezrodriguez28@gmail.com>
2023-03-20 15:05:26 -0500

    cambios en las busquedas

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,19 +1,19 @@
 import random
+import time
 
 def busqueda_binaria(lista,comienzo,final,objetivo):
-
-
+    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final-1]}')
     if comienzo > final:
         return False
-    
     medio = (comienzo + final)// 2
-
     if lista[medio] == objetivo:
         return True
     elif lista[medio] < objetivo:
         return busqueda_binaria(lista,medio + 1, final, objetivo)
     else:
         return busqueda_binaria(lista, comienzo,medio -1, objetivo)
+    
+    
 
 
 
@@ -21,9 +21,9 @@ if __name__ == '__main__':
     tamaño_de_lista = int(input('De que tamaño sera la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = sorted([random.randint(0,100) for i in range(tamaño_de_lista)])
+    lista = sorted([random.randint(0,1000000) for i in range(tamaño_de_lista)])
 
-
-    encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
-    print(lista)
+    comienzo = time.time()
+    encontrado, final = busqueda_binaria(lista, 0, len(lista), objetivo) ,time.time()
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    print(final-comienzo)
--- busqueda_lineal.py
+++ busqueda_lineal.py
@@ -1,5 +1,6 @@
 import random
 
+
 def busqueda_lineal(lista, objetivo):
     match =False
 
@@ -14,9 +15,9 @@ if __name__ == '__main__':
     tamaño_de_lista = int(input('De que tamaño sera la lista? '))
     objetivo = int(input('Que numero quieres encontrar? '))
 
-    lista = [random.randint(0,100) for i in range(tamaño_de_lista)]
+    lista = [random.randint(0,1000000) for i in range(tamaño_de_lista)]
 
-
+    
     encontrado = busqueda_lineal(lista, objetivo)
-    print(lista)
     print(f'El elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    
